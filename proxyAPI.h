ced700b0dd15c8bf8d87b8de7612fb6c7ee1d64d
Zeynn <98015715+Zeynnz@users.noreply.github.com>
2025-01-16 00:13:06 +0100

    Delete proxyAPI.h

--- proxyAPI.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROXYAPI_H
-#define PROXYAPI_H
-
-#define BUFFER_SIZE 1024
-
-void handleFTP(int clientSock);
-void handlePortCommand(int clientSock, int serverSock, char *buffer);
-
-#endif //PROXYAPI_H
